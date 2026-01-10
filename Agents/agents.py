3121e4c503c11cd66f5d5b6d4fe8708f41b98f6e
shlok728 <shloksingh612@gmail.com>
2026-01-10 22:09:10 +0530

    Add Assistant class for livekit integration
    Implemented an Assistant class that integrates with livekit and various tools.

--- Agents/agents.py
+++ Agents/agents.py
@@ -1,1 +1,54 @@
+from dotenv import load_dotenv
 
+from livekit import agents, rtc
+from livekit.agents import AgentServer, AgentSession, Agent,RoomInputOptions
+from livekit.plugins import (
+    noise_cancellation,
+)
+from livekit.plugins import google
+from prompts import AGENT_INSTRUCTIONS, SESSION_INSTRUCTIONS
+from tools import get_weather, search_web, send_email
+load_dotenv()
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions=AGENT_INSTRUCTIONS,
+            llm=google.beta.realtime.RealtimeModel(
+                voice="charon",
+                temperature=0.8
+            ),
+            tools=[
+            get_weather, 
+            search_web,
+            send_email,
+            ]
+        )
+
+server = AgentServer()
+
+
+async def entrypoint(ctx: agents.JobContext):
+    session = AgentSession(
+        
+    )
+    
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            video_enabled=True,
+            noise_cancellation= noise_cancellation.BVC(),
+            
+        ),
+    )
+    await ctx.connect()
+
+    await session.generate_reply(
+        instructions=SESSION_INSTRUCTIONS,
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
